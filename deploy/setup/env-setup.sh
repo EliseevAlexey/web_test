ca2d1f8836dc0d31b439e2e3fce613dadd9464af
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-01 20:27:26 -0700

    one click setup

--- deploy/setup/env-setup.sh
+++ deploy/setup/env-setup.sh
@@ -1,22 +1,35 @@
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+blue() {
+  printf "${BLUE}$@${NC}\n"
+}
+
+exec() {
+  COMMANDS=$@
+  blue $COMMANDS
+  $COMMANDS
+}
+
 # Docker https://docs.docker.com/engine/install/ubuntu/
 APP_NAME='docker'
 if ! command -v $APP_NAME &> /dev/null
 then
     echo "-- Installing '$APP_NAME'"
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl gnupg
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    echo \
+    exec sudo apt-get update
+    exec sudo apt-get install -y ca-certificates curl gnupg
+    exec sudo install -m 0755 -d /etc/apt/keyrings
+    exec curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    exec sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    exec echo \
       "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
-    newgrp docker
+    exec sudo apt update
+    exec sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#    exec sudo groupadd docker
+#    exec sudo usermod -aG docker $USER
 else
     echo "-- '$APP_NAME' already installed, skipping"
 fi
@@ -26,16 +39,16 @@ APP_NAME='microk8s'
 if ! command -v $APP_NAME &> /dev/null
 then
     echo "-- Installing '$APP_NAME'"
-    sudo apt-get update
-    sudo snap install microk8s --classic --channel=1.27
-    sudo usermod -a -G microk8s $USER
-    sudo chown -f -R $USER ~/.kube
-    sudo su - $USER
-    microk8s status --wait-ready
-    microk8s start
-    microk8s enable registry ingress
-    echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
-    source ~/.bashrc
+    exec sudo apt-get update
+    exec sudo snap install microk8s --classic --channel=1.27
+    exec sudo usermod -a -G microk8s $USER
+    exec sudo chown -f -R $USER ~/.kube
+    exec sudo su - $USER
+    exec microk8s status --wait-ready
+    exec microk8s start
+    exec microk8s enable registry ingress
+    exec echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
+    exec source ~/.bashrc
 else
     echo "-- '$APP_NAME' already installed, skipping"
 fi
