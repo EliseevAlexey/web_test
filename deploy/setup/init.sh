0fc77a24a0c977f17c7abbbf2f8faa9955fb6138
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-01 21:37:01 -0700

    one click setup

--- deploy/setup/init.sh
+++ deploy/setup/init.sh
@@ -4,6 +4,7 @@ source "$ROOT_DIR/deploy/local/local.env"
 
 $SSH_COMMAND "git clone $REPO_PATH && /root/$APP_NAME/deploy/setup/env-setup.sh"
 $SSH_COMMAND "/root/$APP_NAME/deploy/setup/k8s-config.sh"
-$SSH_COMMAND "/root/$APP_NAME/deploy/deploy.sh"
+$SSH_COMMAND "cd /root/$APP_NAME && ./deploy/deploy.sh"
 
+open "http://$SERVER_HOST"
 echo "Setup complete!"
