2e0428aad3eee5e0a9acd683518c288e458072b3
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-01 20:04:38 -0700

    one click setup

--- deploy/setup/init.sh
+++ deploy/setup/init.sh
@@ -2,4 +2,4 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 source "$ROOT_DIR/deploy/local/local.env"
 
-$SSH_COMMAND "git clone $REPO_PATH && cd ./$REPO_NAME && ./deploy/setup/env-setup.sh"
+$SSH_COMMAND "git clone $REPO_PATH && cd ./$REPO_NAME/ && ./deploy/setup/env-setup.sh"
