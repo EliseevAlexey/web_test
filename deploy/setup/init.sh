0db2806a7064f9c004456567f87e8018085740e5
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-06 16:18:16 -0700

    init script

--- deploy/setup/init.sh
+++ deploy/setup/init.sh
@@ -3,9 +3,9 @@ ROOT_DIR="$(git rev-parse --show-toplevel)"
 source "$ROOT_DIR/deploy/local/local.env"
 APP_PATH="/root/$APP_NAME"
 
-# $SSH_COMMAND "git clone $REPO_PATH && $APP_PATH/deploy/setup/env-setup.sh"
-# $SSH_COMMAND "$APP_PATH/deploy/setup/k8s-config.sh"
-# $SSH_COMMAND "cd $APP_PATH && ./deploy/setup/ingress-config.sh"
+$SSH_COMMAND "git clone $REPO_PATH && $APP_PATH/deploy/setup/env-setup.sh"
+$SSH_COMMAND "$APP_PATH/deploy/setup/k8s-config.sh"
+$SSH_COMMAND "cd $APP_PATH && ./deploy/setup/ingress-config.sh"
 $SSH_COMMAND "cd $APP_PATH && ./deploy/deploy.sh"
 
 open "http://$SERVER_HOST"
