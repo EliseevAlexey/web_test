2e9fe19cfa094fb8bba38feff99f38c47f66363d
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-06 16:29:06 -0700

    t

--- deploy/setup/init.sh
+++ deploy/setup/init.sh
@@ -1,11 +1,11 @@
 # Load env variables
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 source "$ROOT_DIR/deploy/local/local.env"
-APP_PATH="/root/$APP_NAME"
+SERVER_APP_PATH="/root/$APP_NAME"
 
-# $SSH_COMMAND "git clone $REPO_PATH && $APP_PATH/deploy/setup/env-setup.sh"
-$SSH_COMMAND "cd REPO_PATH && ./deploy/setup/k8s-config.sh"
-$SSH_COMMAND "cd $APP_PATH && ./deploy/deploy.sh"
+# $SSH_COMMAND "git clone $REPO_PATH && $SERVER_APP_PATH/deploy/setup/env-setup.sh"
+$SSH_COMMAND "cd $SERVER_APP_PATH && ./deploy/setup/k8s-config.sh"
+$SSH_COMMAND "cd $SERVER_APP_PATH && ./deploy/deploy.sh"
 
 open "http://$SERVER_HOST"
 echo "Setup complete!"
