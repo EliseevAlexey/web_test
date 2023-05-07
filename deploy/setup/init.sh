c0223e98c184ca993aca152064ad772c53c87fbe
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-06 17:15:14 -0700

    t

--- deploy/setup/init.sh
+++ deploy/setup/init.sh
@@ -4,8 +4,8 @@ source "$ROOT_DIR/deploy/local/local.env"
 SERVER_APP_PATH="/root/$APP_NAME"
 
 # $SSH_COMMAND "git clone $REPO_PATH && $SERVER_APP_PATH/deploy/setup/env-setup.sh"
-$SSH_COMMAND "cd $SERVER_APP_PATH && ./deploy/setup/k8s-config.sh"
-# $SSH_COMMAND "cd $SERVER_APP_PATH && ./deploy/deploy.sh"
+# $SSH_COMMAND "cd $SERVER_APP_PATH && ./deploy/setup/k8s-config.sh"
+$SSH_COMMAND "cd $SERVER_APP_PATH && ./deploy/deploy.sh"
 
 # open "http://$SERVER_HOST"
 # echo "Setup complete!"
