daf8f6d070a324d6bc42a8daa8a1d59b43de8ef7
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-07 07:23:31 -0700

    test

--- deploy/local/push.sh
+++ deploy/local/push.sh
@@ -2,5 +2,5 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 source "$ROOT_DIR/deploy/local/local.env"
 
-git add . && git commit -m "$@" && git push
+git add . && git commit -m "$@" && git push -f
 $SSH_COMMAND 'cd ./$APP_NAME/deploy && git pull && ./deploy.sh'
