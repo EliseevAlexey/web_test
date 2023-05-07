18e00c0b910bd9f97d48bc7dc9835c730fdf31eb
Alexey Eliseev <eliseev.co@gmail.com>
2023-05-07 07:23:19 -0700

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -17,8 +17,8 @@ APP_HELM_VALUES="${GIT_ROOT_DIR}/$APP_NAME/k8s/values/$ENV.yaml"
 blue "docker build -q $APP_DIR -t $REPOSITORY_URL/$APP_NAME:$APP_TAG"
 docker build -q $APP_DIR -t $REPOSITORY_URL/$APP_NAME:$APP_TAG
 
-blue "docker push -q $REPOSITORY_URL/$APP_NAME:$APP_TAG"
-docker push -q $REPOSITORY_URL/$APP_NAME:$APP_TAG
+blue "docker push $REPOSITORY_URL/$APP_NAME:$APP_TAG"
+docker push $REPOSITORY_URL/$APP_NAME:$APP_TAG
 
 blue "microk8s ctr image pull $REPOSITORY_URL/$APP_NAME:$APP_TAG"
 microk8s ctr image pull $REPOSITORY_URL/$APP_NAME:$APP_TAG
